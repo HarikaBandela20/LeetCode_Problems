30cc39b3e5f465fb5ecb18edf6f4966897a1c4ec
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-08-08 11:05:43 -0700

    Time: 2 ms (100%), Space: 44.4 MB (99.54%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<TreeNode> x = new ArrayList<>();
+    int count=0;
+    public void addnodes(TreeNode root, int key) {
+        if (root == null) {
+            return;
+        }
+        addnodes(root.left, key);
+        if (root.val != key) {
+            x.add(root);
+        }
+        addnodes(root.right, key);
+    }
+    public TreeNode convertToBST(List<TreeNode> nodes) {
+        if (nodes.isEmpty()) {
+            return null;
+        }
+        int mid = nodes.size() / 2;
+        TreeNode root = nodes.get(mid);
+        root.left = convertToBST(nodes.subList(0, mid));
+        root.right = convertToBST(nodes.subList(mid + 1, nodes.size()));
+        return root;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        addnodes(root, key);
+        return convertToBST(x);
+    }
+}
