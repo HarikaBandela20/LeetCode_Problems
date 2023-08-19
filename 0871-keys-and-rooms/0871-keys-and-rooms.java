35c84f81ddb310707f7b49ebebe8cd69a57d7599
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-08-19 16:43:10 -0700

    Time: 6 ms (6.35%), Space: 43.2 MB (68.33%) - LeetHub

---
+++ 0871-keys-and-rooms/0871-keys-and-rooms.java
@@ -0,0 +1,34 @@
+class Solution {
+    HashMap<Integer,String> h=new HashMap<Integer,String>();
+    public void scan(List<List<Integer>> rooms1, List<Integer> room){
+        if(room.size()==0){
+            return;
+        }
+        for(int i:room){
+            if(h.get(i)!="true"){
+                h.put(i,"true");
+                List<Integer> x1=new ArrayList<Integer>(rooms1.get(i));
+                scan(rooms1,x1);
+            }
+        }
+    }
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        h.put(0,"true");
+        for(int i=0;i<rooms.size();i++){
+            List<Integer> x=new ArrayList<Integer>(rooms.get(i));
+            for(int j:x){
+                if(!h.containsKey(j)){
+                    h.put(j,"false");
+                }
+            }
+        }
+        scan(rooms,rooms.get(0));
+        //System.out.println(h);
+        for(int i:h.keySet()){
+            if(h.get(i)=="false"){
+                return false;
+            }
+        }
+        return true;
+    }
+}
