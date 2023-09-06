21ebaad6f217a767156ccfbb927228716ebe901d
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-09-06 11:28:12 -0700

    Time: 0 ms (100%), Space: 43.6 MB (53.71%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -11,33 +11,29 @@ class Solution {
     }
 
     public ListNode[] splitListToParts(ListNode head, int k) {
-        // Check if head is null
         if (head == null) {
             ListNode[] emptyParts = new ListNode[k];
-            return emptyParts; // Return an array of k empty lists
+            return emptyParts;
         }
-
         countelements(head);
 
         int capacity = count / k;
         int remainder = count % k;
 
         ListNode[] res = new ListNode[k];
-        ListNode current = head; // A reference to the current head
+        ListNode current = head; 
 
         for (int i = 0; i < k; i++) {
-            res[i] = current; // Store the current head of the part
-            int partSize = capacity + (i < remainder ? 1 : 0); // Calculate the size of this part
-
-            // Traverse the part and set the next part's head to null
+            res[i] = current; 
+            int partSize = capacity + (i < remainder ? 1 : 0); 
             for (int j = 0; j < partSize - 1; j++) {
                 current = current.next;
             }
 
             if (current != null) {
                 ListNode nextHead = current.next;
-                current.next = null; // Set the next pointer to null to end the current part
-                current = nextHead; // Move to the next part's head
+                current.next = null; 
+                current = nextHead; 
             }
         }
 
