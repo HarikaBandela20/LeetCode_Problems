fae39d3d144e43dca12b83dfe450c7312d94d710
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-09-10 23:26:55 -0700

    Time: 0 ms (100%), Space: 41 MB (61.51%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,22 +1,12 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k=0;
-        Arrays.sort(nums);
-        for(int i:nums){
-            if(i==val){
-                k++;
+        int newIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[newIndex] = nums[i];
+                newIndex++;
             }
         }
-        int last=nums.length-1;
-        for(int i=0;i<nums.length-k;i++){
-           if(nums[i]==val){
-               int temp=nums[last];
-               nums[last]=nums[i];
-               nums[i]=temp;
-               last--;
-           } 
-           System.out.println(Arrays.toString(nums));
-        }
-        return nums.length-k;
+        return newIndex; 
     }
 }
