7274fcc33ece84e47bcc1f7b0150cc2aeb2c0adc
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-09-17 19:42:41 -0700

    Time: 6 ms (8.03%), Space: 45 MB (9.56%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
         int a[]=new int[mat.length];
+        HashMap<Integer,List<Integer>> h=new HashMap<>();
         ArrayList<Integer> m=new ArrayList<Integer>();
         for(int i=0;i<mat.length;i++){
             int count=0;
@@ -13,12 +14,20 @@ class Solution {
             if(!m.contains(count)){
                 m.add(count);
             }
-            System.out.println(a[i]);
+            if(!h.containsKey(a[i])){
+                ArrayList<Integer> t=new ArrayList<Integer>();
+                t.add(i);
+                h.put(a[i],t);
+            }else{
+                ArrayList<Integer> t=new ArrayList<Integer>(h.get(a[i]));
+                t.add(i);
+                h.put(a[i],t);
+            }
         }
+        System.out.println(h);
         int res[]=new int[k];
         int index=0;
         Collections.sort(m);
-        System.out.println(m);
         for(int j:m){
             for(int i=0;i<a.length;i++){
                 if(index==k){
