d9619bed95aa7a59f9714835069124db692ceaa8
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-05 10:12:54 -0700

    Time: 2 ms (98.52%), Space: 47.3 MB (5.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,17 +1,51 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int cap=nums.length/3;
-        HashMap<Integer,Integer> x=new HashMap<Integer,Integer>();
         List<Integer> res=new ArrayList<Integer>();
+        int count1=0;
+        int count2=0;
+        int fn=0;
+        int sn=0;
         for(int i:nums){
-            if(!res.contains(i)){
-                x.put(i, x.getOrDefault(i, 0)+1);
-            }
-            if(x.get(i)>cap && !res.contains(i)){
-                res.add(i);
+            if(fn!=0 && i==fn){
+                count1++;
+            }else if(sn!=0 && i==sn){
+                count2++;
+            }else if(count1==0){
+                count1=1;
+                fn=i;
+            }else if(count2==0){
+                count2=1;
+                sn=i;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        //System.out.println(x);
+        count1=0;
+        count2=0;
+        for(int i:nums){
+            if(i==fn){
+                count1++;
+            }
+            else if(i==sn){
+                count2++;
+            }
+        }
+        //System.out.println(fn+" & "+sn);
+        int cap=(nums.length)/3;
+        if(fn==sn){
+            if(count1+count2>cap){
+                res.add(fn);
+                return res;
+            }
+        }else{
+            if(count1>cap){
+                res.add(fn);
+            }
+            if(count2>cap){
+                res.add(sn);
+            }
+        }
         return res;
     }
 }
