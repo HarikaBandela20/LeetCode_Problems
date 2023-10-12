c97bbfcc4a0464589cf3003f72ffd252319a7e3b
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-11 17:25:59 -0700

    Time: 16 ms (27.86%), Space: 44.8 MB (9.9%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,16 +1,20 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] x=s.split(" ");
+        StringBuilder s1=new StringBuilder();
         String res="";
-        for(int i=0;i<x.length;i++){
-            String temp=x[i];
-            for(int j=temp.length()-1;j>=0;j--){
-                res=res+temp.charAt(j);
-            }
-            if(i<x.length-1){
-                res=res+" ";
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c==' '){
+                s1.reverse();
+                s1.append(" ");
+                res=res+s1.toString();
+                s1.delete(0,s1.capacity());
+            }else{
+                s1.append(c);
             }
         }
+        s1.reverse();
+        res=res+s1.toString();
         return res;
     }
 }
