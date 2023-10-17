1cd957b0fefafaa937835605b296d0937148cbf9
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-16 20:22:29 -0700

    Time: 15 ms (88.17%), Space: 46 MB (96.13%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -0,0 +1,28 @@
+class MyHashMap {
+    HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
+    public MyHashMap() {
+    }
+    
+    public void put(int key, int value) {
+        h.put(key,value);
+    }
+    
+    public int get(int key) {
+        if(h.containsKey(key)){
+            return h.get(key);
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        h.remove(key);
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
