2740e3bf427ba6e43201edc1a527bbbd45412c04
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-23 11:41:05 -0700

    Time: 1 ms (76.31%), Space: 39.2 MB (83.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        int r=0;
-        while(n>4){
-            r=n%4;
-            if(r!=0){
-                return false;
-            }
-            n=n/4;
+        if(n==1){
+            return true;
         }
-        if(r==0 && (n==4 || n==1)){
+        if(n<=0){
+            return false;
+        }
+        double x=Math.log(n)/Math.log(4);
+        if(x==(int)x){
             return true;
         }
         return false;
