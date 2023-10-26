a8e78a246cabb367f4ae9dbccb53b74866ffa4df
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-25 18:05:27 -0700

    Time: 20 ms (25.87%), Space: 58.3 MB (5.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> h=new HashSet<Integer>();
-        for(int i:nums){
-            h.add(i);
-        }
-        if(nums.length!=h.size()){
-            return true;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
         return false;
     }
