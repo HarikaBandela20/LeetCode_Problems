1ac91fc5512c43b52613e0a4e94e75331a92e865
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-30 18:39:49 -0700

    Time: 14 ms (19.8%), Space: 44.4 MB (5.72%) - LeetHub

---
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,47 @@
+class Solution {
+    HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
+    HashMap<Integer,ArrayList<Integer>> h1=new HashMap<Integer,ArrayList<Integer>>();
+    public int[] sortByBits(int[] arr) {
+        Arrays.sort(arr);
+        int[] ans=new int[arr.length];
+        for(int i:arr){
+            int t=i;
+            int count=0;
+            if(i==0){
+                h.put(i,0);
+            }
+            else{
+                while(i>0){
+                    if(h.containsKey(i)){
+                        count=count+h.get(i);
+                        break;
+                    }
+                    count=count+(i%2);
+                    i=i/2;
+                }
+                h.put(t,count);
+            }
+        }
+        for(int i:arr){
+            if(h1.containsKey(h.get(i))){
+                ArrayList<Integer> temp=new ArrayList<Integer>(h1.get(h.get(i)));
+                temp.add(i);
+                h1.put(h.get(i),temp);
+            }else{
+                ArrayList<Integer> temp=new ArrayList<Integer>();
+                temp.add(i);
+                h1.put(h.get(i),temp);
+            }
+        }
+        //System.out.println(h1);
+        int c=0;
+        for(int i:h1.keySet()){
+            ArrayList<Integer> temp=new ArrayList<Integer>(h1.get(i));
+            for(int j:temp){
+                ans[c]=j;
+                c++;
+            }
+        }
+        return ans;
+    }
+}
