1f600a71feaf5956459ab57e1de2988afde2426b
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-10-30 18:50:44 -0700

    Time: 10 ms (42.76%), Space: 44.4 MB (5.72%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.java
@@ -1,41 +1,36 @@
 class Solution {
-    HashMap<Integer,Integer> h=new HashMap<Integer,Integer>();
-    HashMap<Integer,ArrayList<Integer>> h1=new HashMap<Integer,ArrayList<Integer>>();
+    HashMap<Integer,ArrayList<Integer>> h=new HashMap<Integer,ArrayList<Integer>>();
     public int[] sortByBits(int[] arr) {
-        Arrays.sort(arr);
         int[] ans=new int[arr.length];
         for(int i:arr){
             int t=i;
-            int count=0;
             if(i==0){
-                h.put(i,0);
+                ArrayList<Integer> a=new ArrayList<Integer>();
+                a.add(i);
+                h.put(0,a);
             }
             else{
+                int count=0;
                 while(i>0){
-                    if(h.containsKey(i)){
-                        count=count+h.get(i);
-                        break;
-                    }
                     count=count+(i%2);
                     i=i/2;
                 }
-                h.put(t,count);
-            }
-        }
-        for(int i:arr){
-            if(h1.containsKey(h.get(i))){
-                ArrayList<Integer> temp=new ArrayList<Integer>(h1.get(h.get(i)));
-                temp.add(i);
-                h1.put(h.get(i),temp);
-            }else{
-                ArrayList<Integer> temp=new ArrayList<Integer>();
-                temp.add(i);
-                h1.put(h.get(i),temp);
+                if(h.containsKey(count)){
+                    ArrayList<Integer> a=new ArrayList<Integer>(h.get(count));
+                    a.add(t);
+                    h.put(count,a);
+                }else{
+                    ArrayList<Integer> a=new ArrayList<Integer>();
+                    a.add(t);
+                    h.put(count,a);
+                }
             }
         }
         int c=0;
-        for(int i:h1.keySet()){
-            ArrayList<Integer> temp=new ArrayList<Integer>(h1.get(i));
+        //System.out.println(h);
+        for(int i:h.keySet()){
+            ArrayList<Integer> temp=new ArrayList<Integer>(h.get(i));
+            Collections.sort(temp);
             for(int j:temp){
                 ans[c]=j;
                 c++;
