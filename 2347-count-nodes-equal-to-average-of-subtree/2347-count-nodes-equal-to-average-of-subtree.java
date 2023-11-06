fae9588e675ce8370548f39fad413261315284b8
Harika Bandela <124243226+HarikaBandela20@users.noreply.github.com>
2023-11-06 00:33:28 -0700

    Time: 1 ms (41.54%), Space: 43.2 MB (22.96%) - LeetHub

--- 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
+++ 2347-count-nodes-equal-to-average-of-subtree/2347-count-nodes-equal-to-average-of-subtree.java
@@ -1,51 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    Stack<Integer> s=new Stack<Integer>();
-    Stack<Integer> n=new Stack<Integer>();
-    Stack<Integer> e=new Stack<Integer>();
-    public int sum(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int t=sum(root.left)+sum(root.right);
-        s.push(root.val+t);
-        n.add(root.val);
-        return root.val+t;
-    }
-    public int count(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int t=count(root.left)+count(root.right)+1;
-        e.push(t);
-        return t;
-    }
+    int ans=0;
     public int averageOfSubtree(TreeNode root) {
-        sum(root);
-        count(root);
-        int ans=0;
-        while(!s.isEmpty()){
-            int i=s.pop();
-            int j=e.pop();
-            int k=n.pop();
-            if(Math.floor(i/j)==k || Math.ceil(i/j)==k){
-                ans++;
-            }
-        }
+        int[] result = dfs(root);
         return ans;
+    }
+    public int[] dfs(TreeNode node) {
+        if (node == null) {
+            return new int[]{0, 0}; // {sum, count}
+        }
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+        int sum = left[0] + right[0] + node.val;
+        int count = left[1] + right[1] + 1;
+        if(Math.floor(sum/count)==node.val || Math.ceil(sum/count)==node.val){
+            ans++;
+        }
+        if (sum % count == 0) {
+            return new int[]{sum, count};
+        } else {
+            return new int[]{sum, count};
+        }
     }
 }
